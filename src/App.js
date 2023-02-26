6d5b82d1908a7c389f54146515d3637fd6d1a4c1
Sasha <faty422@mail.ru>
2023-02-26 22:20:56 +0300

    Использование JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let number = 0;
+	return (
+		<div className="App">
+			<h1>{number}</h1>
+			<button onclick={() => number += 1}>Увеличить</button>
+			<button onclick={() => number -= 1}>Уменьшить</button>
+		</div>
+	);
 }
 
 export default App;
